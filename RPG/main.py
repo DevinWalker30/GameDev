1c7298571a928868e2f0d12e5d64e5578cd06522
DevinWalker30 <devinh2307@gmail.com>
2025-01-07 13:32:37 -0500

    commit

--- RPG/main.py
+++ RPG/main.py
@@ -11,8 +11,19 @@ def game_play():
     clock = pg.time.Clock()
     playing = True
 
-    tilemap = spriteSheet('RPG\imgs\tilemap.png')
-    bow_img = tilemap.get_img(16*10, 16*11, 16, 16)
+    tilemap = spriteSheet('RPG\imgs\\tilemap.png')
+    map_list = []
+
+    for y in range(4):
+        for x in range(3):
+            xloc = 16*x + x
+            yloc = 16*y + y
+            map_img = tilemap.get_img(xloc, yloc, 16, 16, scalex=15, scaley=15)
+
+            map_img.set_colorkey(BLACK)
+            map_list.append(map_img)
+    scale = 15
+
     explosion_sheet = spriteSheet('RPG\imgs\explosion.png')
     explosion_list = []
 
@@ -26,6 +37,25 @@ def game_play():
             explosion_list.append(image)
 
 
+
+    char_sheet = spriteSheet('RPG\imgs\spritesheet_characters.png')
+    char_uzi = char_sheet.get_img(0, 0, 57, 43)
+    char_pistol = char_sheet.get_img(57*2, 0, 52, 43)
+    char_rifle = char_sheet.get_img(57*2-4, 43*3+3, 53, 43)
+    char_holding = char_sheet.get_img(53*5-1, 43, 43, 43)
+    char_up = char_sheet.get_img(57*6+7, 0, 39, 43)
+    char_down = char_sheet.get_img(57*6+11, 43*3+3, 33, 43)
+    
+    
+    char_uzi.set_colorkey(BLACK)
+    char_pistol.set_colorkey(BLACK)
+    char_rifle.set_colorkey(BLACK)
+    char_holding.set_colorkey(BLACK)
+    char_up.set_colorkey(BLACK)
+    char_down.set_colorkey(BLACK)
+
+
+
     while playing:
    
         for event in pg.event.get():
@@ -35,7 +65,24 @@ def game_play():
         screen.fill(CAVE_GREY)
     
 
-        screen.blit(explosion_list[0], (100, 500))
+        # screen.blit(explosion_list[0], (100, 500))
+
+        for i in range(len(map_list)):
+            if i<3:
+                screen.blit(map_list[i], (i*16*scale, 0))
+            elif i<6:
+                screen.blit(map_list[i], (i*16*scale - (3*16*scale), (scale*16)))
+            elif i<9:
+                screen.blit(map_list[i], (i*16*scale - (6*16*scale), (scale*32)))
+            elif i<12:
+                screen.blit(map_list[i], (i*16*scale - (9*16*scale), (scale*48)))
+
+        screen.blit(char_uzi, (100, 100))
+        screen.blit(char_pistol, (200, 100))
+        screen.blit(char_rifle, (300, 100))
+        screen.blit(char_holding, (400, 100))
+        screen.blit(char_up, (500, 100))
+        screen.blit(char_down, (600, 100))
 
         pg.display.flip()
 
--- RPG/sprites.py
+++ RPG/sprites.py
@@ -6,7 +6,9 @@ class spriteSheet():
     def __init__(self, filename):
         self.spritesheet = pg.image.load(filename).convert()
 
-    def get_img(self, x, y, width, height):
+    def get_img(self, x, y, width, height, scalex=None, scaley=None):
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0,0), (x, y, width, height))
+        if scalex and scaley:
+            image = pg.transform.scale(image, (width*scalex, height*scaley))
         return image
